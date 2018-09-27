8ddaaed10f3e267cd177743f2fe691ce50b593d7
limcuili <limcuili@hotmail.com>
2018-09-27 12:25:36 +0100

    created CharCountB class, account for singular and multiple lines

---
+++ CharCountB.java
@@ -0,0 +1,19 @@
+class CharCountB {
+
+    static final int LINE_WIDTH = 80;
+
+    public static void main (String[] args) {
+        if (args.length < 1) {
+            return;
+        }
+        int length = args[0].length();
+        int lines = ( (length - 1) / LINE_WIDTH) + 1;
+        System.out.println("has " + length + " characters");
+        String output = "needs " + lines + " line";
+        if (lines > 1) {
+            output += "s";
+        }
+        System.out.println(output);
+    }
+
+}
