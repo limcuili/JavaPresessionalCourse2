797c2f4750cb7a4264b64c5b84820d47fd4246c1
limcuili <limcuili@hotmail.com>
2018-09-27 11:54:19 +0100

    changed name of integerArray to follow convention of constants being capitalized

--- ManyTimes.java
+++ ManyTimes.java
@@ -5,10 +5,10 @@ class ManyTimes {
             System.out.println("Please give an argument in the command line upon running.");
         } else {
             String newLine = System.getProperty("line.separator");
+            int[] INTEGER_ARRAY = {3,9,10};
             String s = args[0];
-            int[] integerArray = {3,9,10};
-            for(int i = 0; i < integerArray.length; ++i) {
-                for(int j = 0; j < integerArray[i]; ++j) {
+            for(int i = 0; i < INTEGER_ARRAY.length; ++i) {
+                for(int j = 0; j < INTEGER_ARRAY[i]; ++j) {
                     System.out.print(s + " ");
                 }
                 System.out.print(newLine);
